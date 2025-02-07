02bf0fab86ea4550d3ed96ddb7c9bbe28012070f
gaurabMagar <gdikshan10@gmail.com>
2025-02-07 21:54:37 +0100

    asset and component added succesfully

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="flex items-center justify-center">
+        <h1 className="text-amber-400 text-2xl">My App</h1>
+        <p>My App is a React app.</p>
+        <h5 className="text-green-300">my name is gaurab thapa magar</h5>
+      </div>
+    </>
+  )
+}
+
+export default App
